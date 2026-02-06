97281e203d1529edd9e8332ff284ed03dd3ad07c
josephmatts <josephmathew2001@gmail.com>
2026-02-06 08:09:16 +0530

    wip

--- spi_master.v
+++ spi_master.v
@@ -11,20 +11,79 @@ module i_spi_master
   input wr_en,
   input [DATA_WIDTH-1:0] din,
   
-  output reg cs,
+  output cs,
   output reg sclk,
   output reg mosi,
   input miso
      
 );
 
+// state
+localparam [1:0]
+    IDLE  = 2'b00,
+    START = 2'b01,  // load registers
+    DATA  = 2'b10, 
+    STOP  = 2'b11;  // reset registers
+reg [1:0] state;
+
+
+reg [DATA_WIDTH-1:0] data_to_send;
+reg [$clog2(DATA_WIDTH):0] bit_counter;
+
+assign cs = state == IDLE; // cs goes high at idle only
+
+// registers and data
+always@(posedge clk) begin
+    if(rst) begin
+        data_to_send <= 0;
+        bit_counter  <= 0;
+    end else begin
+        case (state)
+            : 
+            default: 
+        endcase
+
+
+    end
+end
+
+
+
+// state change logic
+// !need to factor in sclk!
+always@(posedge clk) begin
+    if(rst) begin
+        state <= IDLE;
+    end else begin
+        case(state)
+        
+            IDLE : begin
+                state <= wr_en ? START : IDLE;
+            end
+
+            START : begin
+                state <= DATA;
+            end
+
+            DATA : begin
+                state <= (bit_counter == DATA_WIDTH-1) ? STOP : DATA;
+            end
+
+            STOP : begin
+                state <= IDLE;
+            end
+
+        endcase
+    end
+end
+
 
 // clock divider
 reg [$clog2(CLOCK_DIVIDER+1):0] clk_counter;
 always@(posedge clk) begin
-    if(rst) begin
+    if(rst || state == IDLE) begin
         clk_counter <= 0;
-        sclk        <= 0;
+        sclk        <= CPOL;
     end else begin
         if(clk_counter == CLOCK_DIVIDER-1) begin
             sclk        <= ~sclk;
@@ -34,5 +93,7 @@ always@(posedge clk) begin
         end
     end
 end
+
+
 
 endmodule
