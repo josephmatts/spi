c1e917ad1ecf4e9ccbe1a31c7f38c92212ce5f3e
josephtachlog <joseph@tachlog.com>
2026-02-09 18:18:04 +0530

    spi_master tx done

--- spi_master.v
+++ spi_master.v
@@ -1,98 +1,102 @@
 module spi_master
 #(
-    parameter CLK_DIV       = 2, // >= 2
-    parameter DATA_WIDTH    = 8
-    // mode 0
+    // MODE 0
+    parameter DATA_WIDTH          = 8, 
+    parameter CLOCK_DIVIDER_COUNT = 125     
 )
 (
-  input clk,
-  input rst,
-  input wr_en,
-  input [DATA_WIDTH-1:0] din,
-  
-  output cs,
-  output reg sclk,
-  output mosi      
+    input clk,
+    input reset,
+    input [DATA_WIDTH-1:0] din,
+    input en,
+    
+    output reg cs,
+    output reg sck,
+    output reg mosi 
 );
 
-// SCLK Generator
-reg [$clog2(CLK_DIV)-1:0] clk_cnt;
-reg                       sclk_en;
+// sck generator
+reg [15:0] clock_counter; 
+reg sck_d;
+reg sck_en;
 always@(posedge clk) begin
-    if(rst) begin
-        clk_cnt <= 0;
-        sclk    <= 0;
-    end else if(sclk_en) begin
-        if(clk_cnt == CLK_DIV-1) begin
-            clk_cnt <= 0;
-            sclk    <= ~sclk;
+        sck_d <= sck;
+        if(reset) begin
+            sck    <= 0;
+            sck_d  <= 0;
+            clock_counter <= 0; 
+        end else if(sck_en) begin
+            if(clock_counter == CLOCK_DIVIDER_COUNT-1) begin
+                clock_counter <= 0;
+                sck   <= ~sck;
+            end else begin
+                clock_counter <= clock_counter + 1'b1;
+            end
         end else begin
-            clk_cnt <= clk_cnt + 1;
+            sck <= 0;
         end
-    end else begin
-        clk_cnt <= 0;
-        sclk    <= 0;
-    end
 end
 
+
+// states
+reg [1:0] state;
 localparam [1:0]
     IDLE  = 2'b00,
     START = 2'b01,
     DATA  = 2'b10,
     STOP  = 2'b11;
-reg [1:0] state;
-
-reg [DATA_WIDTH-1:0]       shift_tx;
-reg [$clog2(DATA_WIDTH):0] bit_cnt;
-
-assign cs   = (state == IDLE || state == STOP);
-assign mosi = shift_tx[DATA_WIDTH-1];
-// FSM
-always@(posedge clk or posedge rst) begin
-    if(rst) begin
-        state    <= IDLE;
-        shift_tx <= 0;
-        bit_cnt  <= 0;
-        sclk_en  <= 0;
+    
+reg [DATA_WIDTH-1:0] shift_tx;
+reg [$clog2(DATA_WIDTH)-1:0] bit_counter;
+    
+always@(posedge clk) begin
+    if(reset) begin
+        cs          <= 1;
+        mosi        <= 0;
+        shift_tx    <= 0;
+        bit_counter <= 0;
+        state       <= IDLE;
+        sck_en      <= 0;
     end else begin
-        case(state) 
-        
+        case(state)
+
             IDLE : begin
-               if(wr_en) begin
-                    state    <= START;
-                    shift_tx <= din;
-                    bit_cnt  <= 0;
-               end else begin
-                   state    <= IDLE;
-                   shift_tx <= 0;
-                   bit_cnt  <= 0;
-                   sclk_en  <= 0;
-               end
+                if(en) begin
+                    cs          <= 0;
+                    mosi        <= din[0];
+                    shift_tx    <= din >> 1;
+                    bit_counter <= 0;
+                    state       <= DATA;
+                    sck_en      <= 1;
+                end else begin
+                    cs          <= 1;
+                    mosi        <= 0;
+                    shift_tx    <= 0;
+                    bit_counter <= 0;
+                    state       <= IDLE;
+                    sck_en      <= 0;
+                end   
             end
-        
-            START : begin
-                state   <= DATA;
-                sclk_en <= 1;            
-            end
-            
-            DATA : begin
-               if(bit_cnt == DATA_WIDTH-1) begin
-                    state   <= STOP;
-                    sclk_en <= 0;
-                    bit_cnt <= 0;
-               end else if(sclk && clk_cnt == CLK_DIV-1) begin  // data shifted out on falling edge
-                    shift_tx <= shift_tx << 1;
-                    bit_cnt  <= bit_cnt + 1;
-               end
+                            
+            DATA : begin                
+                if(sck_d && ~sck) begin // negative edge
+                    if(bit_counter == DATA_WIDTH-1) begin
+                        sck_en <= 0;
+                        state  <= STOP;
+                        bit_counter <= 0;
+                    end else begin
+                        mosi        <= shift_tx[0];
+                        shift_tx    <= shift_tx >> 1;
+                        bit_counter <= bit_counter + 1;
+                    end
+                end
             end
             
             STOP : begin
-                state    <= IDLE;
-                shift_tx <= 0;
-                bit_cnt  <= 0;
+                state <= IDLE;
+                cs    <= 1;
             end
-            
-               
+                        
         endcase
     end
 end
